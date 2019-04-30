cc21063e19eea5ed283f2ce9eb6e6c4c0df53f76
Makra Attila <makraat@ext.otpbank.hu>
2019-04-30 08:13:16 +0200

    Time conversion, comparsion functions, eunit test cases.

--- src/wms_common.erl
+++ src/wms_common.erl
@@ -5,7 +5,9 @@
 %% API exports
 -export([timestamp/0,
          elapsed/3,
-         elapsed/2]).
+         elapsed/2,
+         compare/2,
+         convert/2]).
 
 %%====================================================================
 %% API functions
@@ -14,11 +16,63 @@
 %% -----------------------------------------------------------------------------
 %% Time handling functions
 %% -----------------------------------------------------------------------------
+%% @doc
+%%
+%%-------------------------------------------------------------------
+%%
+%% ### Function
+%% timestamp/0
+%% ###### Purpose
+%% Generate current timestamp.
+%% ###### Arguments
+%%
+%% ###### Returns
+%%
+%%-------------------------------------------------------------------
+%%
+%% @end
 -spec timestamp() ->
   {atom(), integer()}.
 timestamp() ->
   {native, erlang:system_time()}.
 
+%% @doc
+%%
+%%-------------------------------------------------------------------
+%%
+%% ### Function
+%% convert/2
+%% ###### Purpose
+%% Convert timestamp for given time unit
+%% ###### Arguments
+%%
+%% ###### Returns
+%%
+%%-------------------------------------------------------------------
+%%
+%% @end
+-spec convert(timestamp(), time_unit()) ->
+  timestamp().
+convert({Unit, _} = Timestamp, Unit) ->
+  Timestamp;
+convert({FromUnit, Value}, ToUnit) ->
+  {ToUnit, erlang:convert_time_unit(Value, FromUnit, ToUnit)}.
+
+%% @doc
+%%
+%%-------------------------------------------------------------------
+%%
+%% ### Function
+%% elapsed/3
+%% ###### Purpose
+%% Calculate elapsed time from timestamps and convert to given time unit.
+%% ###### Arguments
+%%
+%% ###### Returns
+%%
+%%-------------------------------------------------------------------
+%%
+%% @end
 -spec elapsed(timestamp(), timestamp(), time_unit()) ->
   timestamp().
 elapsed({EUnit, Earlier}, {LUnit, Later}, TimeUnit) ->
@@ -26,11 +80,42 @@ elapsed({EUnit, Earlier}, {LUnit, Later}, TimeUnit) ->
    erlang:convert_time_unit(Later, LUnit, TimeUnit) -
      erlang:convert_time_unit(Earlier, EUnit, TimeUnit)}.
 
+%% @doc
+%%
+%%-------------------------------------------------------------------
+%%
+%% ### Function
+%% elapsed/2
+%% ###### Purpose
+%% Calculate elapsed time by using current timestamp, and convert to given
+%% time unit.
+%% ###### Arguments
+%%
+%% ###### Returns
+%%
+%%-------------------------------------------------------------------
+%%
+%% @end
+
 -spec elapsed(timestamp(), time_unit()) ->
   timestamp().
 elapsed(Earlier, TimeUnit) ->
   elapsed(Earlier, timestamp(), TimeUnit).
 
+-spec compare(timestamp(), timestamp()) ->
+  -1 | 0 | 1.
+compare({Unit, Value1}, {Unit, Value2}) ->
+  sgn(Value1 - Value2);
+compare({Unit1, Value1}, {Unit2, Value2}) ->
+  sgn(erlang:convert_time_unit(Value1, Unit1, native) -
+        erlang:convert_time_unit(Value2, Unit2, native)).
+
 %%====================================================================
 %% Internal functions
 %%====================================================================
+-spec sgn(integer()) ->
+  integer().
+sgn(0) ->
+  0;
+sgn(Number) ->
+  Number div abs(Number).
--- test/wms_common_test.erl
+++ test/wms_common_test.erl
@@ -25,5 +25,36 @@ elapsed_test() ->
   Now = wms_common:timestamp(),
 
   {millisecond, ElapsedMsec1} = wms_common:elapsed(Earlier, Now, millisecond),
-  ?assert(ElapsedMsec1 >= Min andalso ElapsedMsec1 =< Max).
+  ?assert(ElapsedMsec1 >= Min andalso ElapsedMsec1 =< Max),
+
+  {microsecond, ElapsedMicro} = wms_common:elapsed(Earlier, Now, microsecond),
+  ?assert(ElapsedMicro >= Min * 1000 andalso ElapsedMicro =< Max * 1000).
+
+compare_test() ->
+  Earlier = wms_common:timestamp(),
+  timer:sleep(500),
+  Later = wms_common:timestamp(),
+
+  % same time unit
+
+  ?assertEqual(-1, wms_common:compare(Earlier, Later)),
+  ?assertEqual(1, wms_common:compare(Later, Earlier)),
+  ?assertEqual(0, wms_common:compare(Earlier, Earlier)),
+
+  % different time unit
+  LaterMsec = wms_common:convert(Later, millisecond),
+
+  ?assertEqual(-1, wms_common:compare(Earlier, LaterMsec)),
+  ?assertEqual(1, wms_common:compare(LaterMsec, Earlier)),
+  ?assertEqual(0, wms_common:compare(Earlier, Earlier)).
+
+convert_test() ->
+  Timestamp = {millisecond, 1000},
+  ?assertEqual(Timestamp, wms_common:convert(Timestamp, millisecond)),
+
+  Expected = {second, 1},
+  ?assertEqual(Expected, wms_common:convert(Timestamp, second)).
+
+
+
 
